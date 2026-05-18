1ae01f13e82e86c0377e1ae7c922698f81288cae
jul00 <jbretana08@gmail.com>
2026-05-18 20:27:20 +0800

    Update jump_state.gd

--- scenes/player/states/jump_state.gd
+++ scenes/player/states/jump_state.gd
@@ -17,15 +17,12 @@ func physics_update(delta: float):
 	if player.input_handler.jump_released() and player.velocity.y < 0: # short hop
 		player.velocity.y = player.jump_velocity / 20
 	
-<<<<<<< Updated upstream
 	if player.input_handler.attack_pressed():
 		state_machine.change_state(state_machine.get_node("AttackHandler"))
 		return
-=======
 	if player.input_buffer.buffered_action == InputBuffer.Action.ATTACK:
 		player.input_buffer.consume()
 		state_machine.change_state(state_machine.get_node("AirNeutralN"))
->>>>>>> Stashed changes
 		
 	if player.velocity.y > 0:
 		state_machine.change_state(state_machine.get_node("JumpTransition"))
